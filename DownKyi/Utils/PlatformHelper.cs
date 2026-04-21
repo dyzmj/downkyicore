7ac4f3830cc63a0df76bb4e2b2fd2e726272293c
姚彪 <28655758+yaobiao131@users.noreply.github.com>
2026-04-21 09:42:20 +0800

    fix: 修复macos打开特殊符号文件失败问题

--- DownKyi/Utils/PlatformHelper.cs
+++ DownKyi/Utils/PlatformHelper.cs
@@ -1,6 +1,9 @@
 using System;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Avalonia.Controls;
+using Avalonia.Platform.Storage;
 using DownKyi.Core.Logging;
 using DownKyi.Events;
 using Prism.Events;
@@ -32,7 +35,7 @@ public static class PlatformHelper
             return;
         }
 
-        _ = await topLevel.Launcher.LaunchFileAsync(openFolder);
+        _ = await BclLauncher.LaunchFileAsync(openFolder);
     }
 
     /// <summary>
@@ -58,7 +61,7 @@ public static class PlatformHelper
             return;
         }
 
-        _ = await topLevel.Launcher.LaunchFileAsync(openFolder);
+        _ = await BclLauncher.LaunchFileAsync(openFolder);
     }
 
     public static async Task OpenUrl(string url, IEventAggregator? eventAggregator = null)
@@ -71,6 +74,93 @@ public static class PlatformHelper
             return;
         }
 
-        _ = await topLevel.Launcher.LaunchUriAsync(new Uri(url));
+        _ = await BclLauncher.LaunchUriAsync(new Uri(url));
+    }
+
+    // https://github.com/AvaloniaUI/Avalonia/pull/19713 avalonia12才修复 暂时使用本地hack
+    private static class BclLauncher
+    {
+        public static Task<bool> LaunchUriAsync(Uri uri)
+        {
+            _ = uri ?? throw new ArgumentNullException(nameof(uri));
+            if (uri.IsAbsoluteUri)
+            {
+                return Task.FromResult(Exec(uri.AbsoluteUri));
+            }
+
+            return Task.FromResult(false);
+        }
+
+        /// <summary>
+        /// This Process based implementation doesn't handle the case, when there is no app to handle link.
+        /// It will still return true in this case.
+        /// </summary>
+        public static Task<bool> LaunchFileAsync(IStorageItem storageItem)
+        {
+            _ = storageItem ?? throw new ArgumentNullException(nameof(storageItem));
+            if (storageItem.TryGetLocalPath() is { } localPath
+                && CanOpenFileOrDirectory(localPath))
+            {
+                return Task.FromResult(Exec(localPath));
+            }
+
+            return Task.FromResult(false);
+        }
+
+        private static bool CanOpenFileOrDirectory(string localPath) => true;
+
+        private static bool Exec(string urlOrFile)
+        {
+            if (OperatingSystem.IsLinux())
+            {
+                // If no associated application/json MimeType is found xdg-open opens return error
+                // but it tries to open it anyway using the console editor (nano, vim, other..)
+                var args = EscapeForShell(urlOrFile);
+                ShellExecRaw($"xdg-open \\\"{args}\\\"", waitForExit: false);
+                return true;
+            }
+            else if (OperatingSystem.IsWindows() || OperatingSystem.IsMacOS())
+            {
+                var info = new ProcessStartInfo
+                {
+                    FileName = OperatingSystem.IsWindows() ? urlOrFile : "open",
+                    CreateNoWindow = true,
+                    UseShellExecute = OperatingSystem.IsWindows()
+                };
+                // Using the argument list avoids having to escape spaces and other special 
+                // characters that are part of valid macos file and folder paths.
+                if (OperatingSystem.IsMacOS())
+                    info.ArgumentList.Add(urlOrFile);
+                using var process = Process.Start(info);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private static string EscapeForShell(string input) => Regex
+            .Replace(input, "(?=[`~!#&*()|;'<>])", "\\")
+            .Replace("\"", "\\\\\\\"");
+
+        private static void ShellExecRaw(string cmd, bool waitForExit = true)
+        {
+            using var process = Process.Start(
+                new ProcessStartInfo
+                {
+                    FileName = "/bin/sh",
+                    Arguments = $"-c \"{cmd}\"",
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                    WindowStyle = ProcessWindowStyle.Hidden
+                }
+            );
+            if (waitForExit)
+            {
+                process?.WaitForExit();
+            }
+        }
     }
 }
